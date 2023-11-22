1d17da6882d46ed7d6d93c0f7584925a09ab4078
Sanjeevvarmabetter <sanjeevvarmacode@gmail.com>
2023-11-22 22:22:29 +0530

    fixed path errors

--- web/blog/models.py
+++ web/blog/models.py
@@ -2,32 +2,27 @@ from django.db import models
 from django.contrib.auth.models import User
 from ckeditor.fields import RichTextField
 
-#to create thumbnails
+# to create thumbnails
 from imagekit.models import ImageSpecField
 from imagekit.processors import ResizeToFill
-# Create your models here.
-
 
 STATUS = (
-    (0,"Draft"),
-    (1,"Publish")
-
+    (0, "Draft"),
+    (1, "Publish")
 )
 
 class Post(models.Model):
     title = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE,related_name='blog_posts')
-    updated_on  = models.DateTimeField(auto_now = True)
-    # content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    updated_on = models.DateTimeField(auto_now=True)
     content = RichTextField()
     image = models.ImageField(upload_to="post_images")
     image_thumbnail = ImageSpecField(source='image',
-                                     processors=[ResizeToFill(100,50)],
+                                     processors=[ResizeToFill(100, 50)],
                                      format='JPEG',
-                                     options={'quality':60})
+                                     options={'quality': 60})
     
-    # thumbnail = models.ImageField(upload_to='thumbnails/', null=True, blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
