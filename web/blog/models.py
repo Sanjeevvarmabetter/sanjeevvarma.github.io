377c37ebc1297b7ecdbf6241c7d4de6ee2bcc72a
Sanjeevvarmabetter <sanjeevvarmacode@gmail.com>
2023-11-29 15:19:32 +0530

    removed thubnail

--- web/blog/models.py
+++ web/blog/models.py
@@ -17,11 +17,11 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     updated_on = models.DateTimeField(auto_now=True)
     content = RichTextField()
-    image = models.ImageField(upload_to="post_images")
-    image_thumbnail = ImageSpecField(source='image',
-                                     processors=[ResizeToFill(100, 50)],
-                                     format='JPEG',
-                                     options={'quality': 60})
+    # image = models.ImageField(upload_to="blog/media/post_images")
+    # image_thumbnail = ImageSpecField(source='image',
+    #                                  processors=[ResizeToFill(100, 50)],
+    #                                  format='JPEG',
+    #                                  options={'quality': 60})
     
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
