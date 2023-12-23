e375a6659c95718537a8d58be70313cf2afd32de
Sanjeevvarmabettter <sanjeevvarmacode@gmail.com>
2023-12-23 17:13:49 +0530

    ready for hosting

--- web/blog/urls.py
+++ web/blog/urls.py
@@ -1,11 +1,11 @@
 from . import views
 from django.urls import path
-from .views import front_page
+
 
 urlpatterns = [
-    path('', front_page, name='front_page'),
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+
 ]
 
 
